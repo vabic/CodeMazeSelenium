9fdf3e91ae761457278ef9d4ea6608531750edd5
vabic <vasile.bicherschi@xpertsea.com>
2020-08-21 16:21:29 -0400

    smth

--- setupDB.sh
+++ setupDB.sh
@@ -1,16 +1,28 @@
+set -e
+
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "install mssql-server\n"
 sudo apt-get update
 sudo apt-get install -y mssql-server
 
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "setup MSSql\n"
 sudo ACCEPT_EULA='Y' MSSQL_PID='Developer' MSSQL_SA_PASSWORD='P@ssw0rd!' MSSQL_TCP_PORT=1433/opt/mssql/bin/mssql-conf setup
 
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "verify if servcie is running\n"
+systemctl status mssql-server --no-pager
+
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "install cli\n"
 #install command line tools
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update 
-sudo apt-get install mssql-tools unixodbc-dev
-sudo apt-get update 
-sudo apt-get install mssql-tools
+sudo ACCEPT_EULA='Y' apt-get install mssql-tools unixodbc-dev
 
-sqlcmd -S localhost -U SA -P 'P@ssw0rd!'
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "connect to database\n"
+sqlcmd -S localhost -U SA -P 'P@ssw0rd!' -Q 'select @@VERSION'
